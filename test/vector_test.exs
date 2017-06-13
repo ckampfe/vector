a66a7ac0f3f82bbd7a10e2e9f6e2c34ab42b0da2
Clark Kampfe <clark.kampfe@gmail.com>
2017-06-13 00:07:43 -0500

    protocol and behaviour test

--- test/vector_test.exs
+++ test/vector_test.exs
@@ -1,4 +1,26 @@
 defmodule VectorTest do
   use ExUnit.Case
   doctest Vector
+
+  setup do
+    list = Enum.to_list(0..100)
+    vec = Vector.new(list)
+
+    %{list: list,
+      vec: vec}
+  end
+
+  test "Access", context do
+    assert context[:vec][999] == nil
+    assert context[:vec][52] == 52
+  end
+
+  test "Collectible", context do
+    assert context[:list] |> Enum.into(Vector.new)
+                          |> Vector.to_list == context[:list]
+  end
+
+  test "Enumerable", context do
+    assert Enum.map(context[:vec], fn(n) -> n + 1 end) == Enum.to_list(1..101)
+  end
 end
