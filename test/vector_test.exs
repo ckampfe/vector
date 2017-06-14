96b1e6a6ab38679e3cfb9d9d77a1e1719b86a126
Clark Kampfe <clark.kampfe@gmail.com>
2017-06-13 22:20:12 -0500

    multidimensional vector test case

--- test/vector_test.exs
+++ test/vector_test.exs
@@ -5,14 +5,17 @@ defmodule VectorTest do
   setup do
     list = Enum.to_list(0..100)
     vec = Vector.new(list)
+    vec_multi = Vector.new([vec, vec, vec])
 
     %{list: list,
-      vec: vec}
+      vec: vec,
+      vec_multi: vec_multi}
   end
 
   test "Access", context do
     assert context[:vec][999] == nil
     assert context[:vec][52] == 52
+    assert context[:vec_multi][1][23] == 23
   end
 
   test "Collectible", context do
